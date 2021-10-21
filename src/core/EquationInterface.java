1e0c5284e471087ed781da154199d14404c1fd21
wallacce135 <wallacce135@gmail.com>
2021-10-21 13:45:08 +0300

    lab 4.1

--- src/core/EquationInterface.java
+++
@@ -1,7 +0,0 @@
-package core;
-
-import java.util.ArrayList;
-
-public interface EquationInterface {
-    public ArrayList<Float> solve(float a, float b, float c) throws Throwable;
-}
--- src/core/LogInterface.java
+++
@@ -1,6 +0,0 @@
-package core;
-
-public interface LogInterface {
-    public LogInterface log(String str);
-    public LogInterface write();
-}
